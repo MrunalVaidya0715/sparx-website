81ff1165c5a7d958feb48efdb4a24d16075f1868
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-05 16:26:40 +0530

    Added admins images

--- sparx-web-app/src/admins.js
+++ sparx-web-app/src/admins.js
@@ -2,52 +2,57 @@ export const AdminsList = [
     {
         id: 1,
         name: "Anne",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/anne.png"
     },
     {
         id: 2,
         name: "Sanket",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/sanket.png"
     },
     {
         id: 3,
         name: "Vanshika",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/vanshika.png"
     },
     {
         id: 4,
         name: "Yash",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/yash.png"
     },
     {
         id: 5,
         name: "leanne",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/leanne.png"
     },
     {
         id: 6,
         name: "Chirac",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/chirac.png"
     },
     {
         id: 7,
         name: "Falguni",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/falguni.png"
     },
     {
         id: 8,
         name: "Osama",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/osama.png"
     },
     {
         id: 9,
         name: "Shubham",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/shubham.png"
     },
     {
         id: 10,
         name: "Anirudha",
-        imgSrc: "/assets/me-back.jpg"
+        imgSrc: "/assets/adminss/anirudha.png"
+    },
+    {
+        id: 11,
+        name: "Sahil",
+        imgSrc: "/assets/adminss/sahil.png"
     },
 
 ]
