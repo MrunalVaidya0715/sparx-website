46027ca5997a09a47bee970e6582126f3280b0be
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-08 22:29:05 +0530

    Added <Analytics /> in _app.js

--- sparx-web-app/src/pages/_app.js
+++ sparx-web-app/src/pages/_app.js
@@ -2,12 +2,13 @@ import '@/styles/globals.css'
 import Navbar from 'components/Navbar'
 import ShadeLight from 'components/ShadeLight'
 import ShadeLight2 from 'components/ShadeLight2'
-
+import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }) {
   return (<>
     <Navbar/>
     
     <Component {...pageProps}/>
+    <Analytics />
   
   </>)
 }
