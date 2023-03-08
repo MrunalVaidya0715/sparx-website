941435bc185331178b6836a18a3b2e2237b934df
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-08 21:05:52 +0530

    Changed Winner Page Desc

--- sparx-web-app/src/pages/winners.jsx
+++ sparx-web-app/src/pages/winners.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const winners = () => {
   return (
     <div className='py-20 flex flex-col justify-center items-center'>
-        <h2 className=' tracking-wider mb-4 font-light '>Schedule</h2>
+        <h2 className=' tracking-wider mb-4 font-light '>Winners 2k23</h2>
     </div>
   )
 }
