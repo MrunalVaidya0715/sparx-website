c4d117654c199d935c9eced0f9a8167e341433b1
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-10 20:15:31 +0530

    added Admins

--- sparx-web-app/components/Admins.jsx
+++ sparx-web-app/components/Admins.jsx
@@ -1,15 +1,43 @@
 import Image from 'next/image'
-import React from 'react'
-import imgMe from '../public/assets/me-back.jpg'
+import React, { useEffect, useRef } from 'react'
 import { AdminsList } from '../src/admins.js'
 const Admins = () => {
+    const containerRef = useRef(null)
+    useEffect(() => {
+        const container = containerRef.current;
+        let scrollInterval;
+    
+        function startScrolling() {
+          scrollInterval = setInterval(() => {
+            if (container.scrollLeft >= container.scrollWidth - container.offsetWidth) {
+                container.scrollLeft = 0;
+              }
+            container.scrollLeft += 1;
+          }, 25);
+        }
+    
+        function stopScrolling() {
+          clearInterval(scrollInterval);
+        }
+    
+        container.addEventListener('mouseenter', stopScrolling);
+        container.addEventListener('mouseleave', startScrolling);
+    
+        startScrolling();
+    
+        return () => {
+          container.removeEventListener('mouseenter', stopScrolling);
+          container.removeEventListener('mouseleave', startScrolling);
+          clearInterval(scrollInterval);
+        };
+      }, []);
     return (
-        <>
+        <div ref={containerRef} className='flex w-full max-w-[1024px] mb-20 overflow-x-scroll no-scrollbar cursor-grab '>
             {
                 AdminsList.map((admin) => {
                     return (
                         <>
-                            <div className='flex flex-col gap-2 justify-between p-2 items-center w-full'>
+                            <div className='flex flex-col gap-2 justify-between p-2 items-center w-full min-w-[12rem]'>
                                 <Image className='  border-4 border-gray-700/50  object-cover mx-4  w-[10rem] h-[10rem] object-top bg-center rounded-full' src={admin.imgSrc} alt="/" width={150} height={150} />
                                 <div>
                                     <p className=' text-[#e6c098] font-semibold '>{admin.name}</p>
@@ -22,7 +50,7 @@ const Admins = () => {
             }
 
 
-        </>
+        </div>
     )
 }
 
