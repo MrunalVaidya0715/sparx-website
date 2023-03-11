358726269a04b9e14a47f07012be1149963c60b4
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-12 00:24:47 +0530

    Added Quick Links

--- sparx-web-app/components/Footer.jsx
+++ sparx-web-app/components/Footer.jsx
@@ -5,9 +5,10 @@ import { FaInstagram } from 'react-icons/fa'
 const Footer = () => {
     return (
         <div className=' flex w-full justify-center items-center mt-20 bg-[#bb2649] p-4'>
+            <p>Developed by: </p>
             <Link href='https://instagram.com/mru.v_?igshid=YmMyMTA2M2Y='>
-            <div className=' cursor-pointer flex items-center'>
-                <p className=' font-thin text-gray-400 text-md'>Developed by: <span className=' text-white'>Mrunal Vaidya</span></p>
+            <div className=' flex items-center'>
+                <p className=' font-thin ml-1 text-gray-400 text-md'><span className=' text-white'> Mrunal Vaidya</span></p>
                 <FaInstagram className=' text-white ml-1' size={20} />
             </div>
             </Link>
--- sparx-web-app/components/HomeLanding.jsx
+++ sparx-web-app/components/HomeLanding.jsx
@@ -1,15 +1,34 @@
+import Link from 'next/link'
 import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
+import { SlBookOpen } from 'react-icons/sl'
 import Slider from './Slider'
 
 const HomeLanding = () => {
+    const brochure = 'https://firebasestorage.googleapis.com/v0/b/fileupload-ff0a8.appspot.com/o/Sparx%20brochure%202022-23.pdf?alt=media&token=2e066469-306d-4bef-bae1-73a84fd71290'
     return (
         <div className=' w-full flex flex-col justify-between items-center'>
             <div className=' pt-20  w-full'>
                 <Slider />
             </div>
             <div className=' max-w-[1240px] w-full h-fit p-4 flex flex-col justify-center items-center'>
-                <h1 className=' text-red-500 uppercase'>Sparx </h1>
+                <h2 className=' text-red-500 uppercase'>Sparx </h2>
                 <h3 className=' font-light text-yellow-200 text-lg tracking-wider'>13th Match Onwards</h3>
+                <div className=' flex w-full justify-center items-center gap-8 mt-2'>
+                <Link href="https://instagram.com/sparx.2023?igshid=YmMyMTA2M2Y=">
+                    <div className='rounded-full bg-gray-700/50 text-white border-[1px] border-gray-700 p-3 cursor-pointer ease-in duration-300'>
+                      <FaInstagram size={24} className="hover:scale-125 transition-all ease-in duration-150" />
+                    </div>
+                  </Link>
+
+                  <Link href={brochure} download>
+                    <div className='rounded-full bg-gray-700/50 border-[1px] border-gray-700 text-white p-3 cursor-pointer ease-in duration-300'>
+                      <SlBookOpen size={24} className="hover:scale-125 transition-all ease-in duration-150" />
+                    </div>
+                  </Link>
+
+
+                </div>
 
 
 
