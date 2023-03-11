7182d6e9d3e80caad19b02d10aa459430495f854
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-11 22:28:20 +0530

    Added Toogle in Winners

--- sparx-web-app/src/pages/winners.jsx
+++ sparx-web-app/src/pages/winners.jsx
@@ -1,17 +1,37 @@
-import Winners from 'components/Winners'
+import { CgToggleSquare, CgToggleSquareOff } from 'react-icons/cg'
 import WinnersTable from 'components/WinnersTable'
-import React from 'react'
+import React, { useState } from 'react'
 
 const winners = () => {
+  const [isIndoor, setIsIndoor] = useState(true)
+  const handleType = () => {
+    setIsIndoor(prev => !prev)
+  }
   return (
     <div className='py-20 flex flex-col justify-center items-center'>
       <h2 className=' tracking-wider mb-4 font-light '>Winners 2<span className=' text-red-500'>K</span>23</h2>
+      {/**Toogle */}
+      <div className=' flex gap-2 items-center justify-center'>
+        <div className={isIndoor ? '' : 'text-gray-700'}>
+          <p>Indoor Sparx</p>
+        </div>
+        <div onClick={handleType} className=' cursor-pointer'>
+          {
+            isIndoor? <CgToggleSquare className=' text-[#BB2649]' size={25} />: <CgToggleSquareOff className=' text-[#BB2649]' size={25} />
+          }
+        </div>
+
+        <div className={isIndoor ? 'text-gray-700' : ''}>
+          <p>Outdoor Sparx</p>
+        </div>
+
+      </div>
       <div className=' max-w-[1240px] w-full h-screen flex items-start justify-center'>
         <div className=' flex flex-col justify-center items-center w-full max-w-[720px]'>
-          
+
           {/**Data loading */}
-          
-          <WinnersTable/>
+
+          <WinnersTable />
 
 
         </div>
