cf274eb71bc6ad99c8fbe028d0595c246c0da2d2
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-12 18:31:42 +0530

    Footer Details

--- sparx-web-app/components/Footer.jsx
+++ sparx-web-app/components/Footer.jsx
@@ -4,11 +4,11 @@ import { FaInstagram } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <div className=' text-gray-500 flex w-full justify-center items-center mt-20 bg-gray-900/90 p-4'>
-            <p>Developed by: </p>
+        <div className=' text-gray-600 flex w-full justify-center items-center mt-20 bg-gray-900/90 p-4'>
+            <p>Designed & Developed by: </p>
             <Link href='https://instagram.com/mru.v_?igshid=YmMyMTA2M2Y='>
             <div className=' flex items-center'>
-                <p className=' font-thin ml-1 text-gray-400 text-md'><span className=' text-white'> Mrunal Vaidya</span></p>
+                <p className=' font-thin ml-1 text-gray-400 text-md'><span className=' text-gray-400'> Mrunal Vaidya</span></p>
                 <FaInstagram className=' text-white ml-1' size={20} />
             </div>
             </Link>
