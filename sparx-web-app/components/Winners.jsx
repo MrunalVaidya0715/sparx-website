84020e2117b3effa7ddb5906ac8d027da631bdb0
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-13 00:45:32 +0530

    Empty Message

--- sparx-web-app/components/Winners.jsx
+++ sparx-web-app/components/Winners.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Winners = () => {
   return (
-    <div className=' flex w-full h-screen justify-center items-center'>
-      <h3 className=' font-light'>Data will be available soon</h3>
+    <div className=' flex w-full h-screen justify-center items-start mt-10'>
+      <h3 className=' font-light'>Empty like your Instagram DMs</h3>
     </div>
   )
 }
