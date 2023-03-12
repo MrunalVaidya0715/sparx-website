0a2df02504928b5cf3b320a617ef8f2cb5dfd115
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-03-13 01:55:48 +0530

    Fixed Footer

--- sparx-web-app/components/Footer.jsx
+++ sparx-web-app/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className=' text-gray-600 flex flex-col w-full justify-center items-center mt-20 bg-gray-900/90 p-4'>
             <p className=' text-gray-500'>Quick Links</p>
             <div className='flex  justify-center items-center mb-4'>
-                <div className=' flex flex-wrap w-full gap-4  md:gap-8 justify-start items-center'>
+                <div className=' flex px-4 flex-wrap w-full gap-4  md:gap-8 justify-center items-center'>
                     <Link href='/'>
                         <p className=' underline underline-offset-2 hover:opacity-[0.4] uppercase text-gray-500'>Home</p>
                     </Link>
@@ -30,7 +30,9 @@ const Footer = () => {
 
             </div>
             <div className=' flex'>
-                <p>Designed & Developed by: </p>
+                <div>
+                    <p>Developed by: </p>
+                </div>
                 <Link href='https://instagram.com/mru.v_?igshid=YmMyMTA2M2Y='>
                     <div className=' flex items-center'>
                         <p className=' font-thin ml-1 text-gray-400 text-md'><span className=' text-gray-400'> Mrunal Vaidya</span></p>
