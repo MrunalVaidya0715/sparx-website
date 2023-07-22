58a3f04bd08bb87676f5eb53b394e531e3ea8e50
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-07-22 17:48:23 +0530

    cleaned

--- sparx-web-app/src/pages/home.jsx
+++ sparx-web-app/src/pages/home.jsx
@@ -1,16 +1,9 @@
-import Admins from 'components/Admins'
-import Heads from 'components/Heads'
+
 import HomeLanding from 'components/HomeLanding'
 import HomeMembers from 'components/HomeMembers'
-import PointsTable from 'components/PointsTable'
-import Slider from 'components/Slider'
-import SlideSwiper from 'components/SlideSwiper'
 import Sponsors from 'components/Sponsors'
-import Table from 'components/Table'
-import Image from 'next/image'
 import React from 'react'
-import Marquee from 'react-fast-marquee'
-import img1 from '../../public/assets/me-back.jpg'
+
 const home = () => {
   return (
     <div className=' flex flex-col justify-center items-center'>
